506b6727d0bd511eb66762cc4c8088efdd358add
DrDeckto <fynjy138@gmail.com>
2022-04-07 13:12:12 +0300

    Добавьте файлы проекта.

---
+++ Homework/CreatorIdForBankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankAccount
+{
+    public static class CreatorIdForBankAccount
+    {
+        /// <summary>
+        /// Генерация Id
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+                
+        }
+    }
+}
