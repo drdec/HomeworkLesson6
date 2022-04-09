651132e7cf57c40eadc5650405903c4a87eaa182
DrDeckto <fynjy138@gmail.com>
2022-04-09 16:48:39 +0300

    Реализован класс точка, окружность

---
+++ Homework/Point.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Homework
+{
+    public abstract class Point : Figure
+    {
+
+        /// <summary>
+        /// Конструктор окружгости
+        /// </summary>
+        /// <param name="color">цвет фигуры</param>
+        /// <param name="isVisible">видимость фигуры</param>
+        /// <param name="center">центр фигуры</param>
+        /// <param name="topOfFigure">левая верхняя вершина фигуры</param>
+        protected Point(Color color, bool isVisible, (int, int) center, (int, int) topOfFigure)
+        {
+            this.color = color;
+            this.isVisible = isVisible;
+            this.center = center;
+            this.topOfFigure = topOfFigure;
+        }
+
+        /// <summary>
+        /// Перемещение фигуры по горизонтали
+        /// </summary>
+        /// <param name="x">на сколько сместить фигуру</param>
+        public void MoveFigureHorizontal(int x)
+        {
+            topOfFigure.Item1 += x;
+            center.Item1 += x;
+        }
+
+        /// <summary>
+        /// смещение фигуры по вертикали
+        /// </summary>
+        /// <param name="y">на сколько сместить фигуру</param>
+        public void MoveFigureVertical(int y)
+        {
+            topOfFigure.Item2 += y;
+            center.Item2 += y;
+        }
+
+        /// <summary>
+        /// Меняет состояние фигуры
+        /// </summary>
+        /// <param name="visible">видимость</param>
+        public virtual void SetVisible(bool visible)
+        {
+            isVisible = visible;
+        }
+
+        /// <summary>
+        /// Меняет цвет фигуры
+        /// </summary>
+        /// <param name="color">цвет фигуры</param>
+        public void SetColorFigure(Color color)
+        {
+            this.color = color;
+        }
+
+        /// <summary>
+        /// Вывод информации об объекте
+        /// </summary>
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Цвет - {color}\n" +
+                              $"Фигура видима - {isVisible}\n" +
+                              $"Центр фигру  - {center}\n" +
+                              $"Координата вершины x - {topOfFigure.Item1}\n" +
+                              $"Координата вершины y - {topOfFigure.Item2}\n");
+        }
+    }
+}
---
+++ Homework/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework
+{
+    public class Rectangle : Point
+    {
+        /// <summary>
+        /// Конструктор окружности
+        /// </summary>
+        /// <param name="color">цвет фигуры</param>
+        /// <param name="isVisible">видимость фигуры</param>
+        /// <param name="center">центр фигуры</param>
+        /// <param name="topOfFigure">левая верхняя вершина фигуры</param>
+        public Rectangle(Color color, bool isVisible, (int, int) center, (int, int) topOfFigure) :
+            base(color, isVisible, center, topOfFigure)
+        {
+
+        }
+
+
+    }
+}
